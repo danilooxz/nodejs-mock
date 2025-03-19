6a5a6fdc81b9f40f57205b1ca26971d613e57bee
Danilo <juniordanilo538@gmail.com>
2025-03-18 22:19:41 -0300

    aula02

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let products = [];
+
+app.get('/', (req, res) => {
+    res.send('Server Online!');
+});
+
+//Busca lista
+app.get('/products', (req, res) => {
+    res.json({ products: products }).send();
+});
+
+//Cria
+app.post('/products', (req, res) => {
+    const { name, marca, tamanho, cor} = req.body;
+    products.push({ id: products.length + 1, name });
+
+    res.status(201).json().send();
+});
+
+//Atualiza varios sem retorno 
+app.put('/products/:id', (req, res) => {
+    
+    res.status(200).json(products).send();
+});
+
+//Atualiza Um
+app.patch('/products/:id', (req, res) => {
+
+    res.status(200).json(products).send();
+});
+
+//Deleta
+app.delete('/products/:id', (req, res) => {
+    const { id } = req.params;
+    products = products.filter(product => product.id != id);
+
+    res.status(200).json(products).send();
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
